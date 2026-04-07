ee1b67aaefe92f16292930063e93d332b023be0f
ynagle <yashnagle98@gmail.com>
2026-04-07 13:29:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import numpy as np
 from ultralytics import YOLO
 import tempfile
 import os
-# 🔥 ADD THIS FIX
-import torch
-from ultralytics.nn.tasks import DetectionModel
-torch.serialization.add_safe_globals([DetectionModel])
 # -------------------------
 # Lazy Load Model (IMPORTANT)
 # -------------------------
